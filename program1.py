d7b34eb3a98f50b2c1adc83796b94a9e51daccb9
A-smita <74916407+A-smita@users.noreply.github.com>
2021-02-05 16:17:00 +0530

    all the programs are in python

---
+++ program1.py
@@ -0,0 +1,41 @@
+#Calculator
+
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b
+
+
+print("Select any operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+
+    choice = (input("Enter choice(1/2/3/4): "))
+
+    
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter the first value: "))
+        num2 = float(input("Enter the second value: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        break
+    else:
+        print("Invalid Input")
